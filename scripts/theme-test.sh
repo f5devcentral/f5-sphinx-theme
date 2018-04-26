07754e7be9a6737ebd56697f6981033529bcbe82
Eric Flores <ericflorescode@gmail.com>
2018-04-26 00:33:34 -0500

    Allow for another test repo

--- scripts/theme-test.sh
+++ scripts/theme-test.sh
@@ -6,7 +6,7 @@ set -x
 set -e
 
 # Clone a test docs repo
-git clone https://github.com/jputrino/test-deploy.git $TRAVIS_BUILD_DIR/test-deploy
+git clone $TEST_DOCS_REPO $TRAVIS_BUILD_DIR/test-deploy
 
 # build some test docs
 make -C test-deploy/docs/ html && mv test-deploy/docs/_build/html $TRAVIS_BUILD_DIR/html
