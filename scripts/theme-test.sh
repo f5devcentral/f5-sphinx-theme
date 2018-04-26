507af6175a5dcf687f9058cba0f04aa9370d495e
Eric Flores <ericflorescode@gmail.com>
2018-04-26 01:03:48 -0500

    Allow for builds from local repo

--- scripts/theme-test.sh
+++ scripts/theme-test.sh
@@ -6,7 +6,9 @@ set -x
 set -e
 
 # Clone a test docs repo
-git clone $TEST_DOCS_REPO $TRAVIS_BUILD_DIR/test-deploy
+if [[ -v TEST_DOCS_REPO ]]; then
+  git clone $TEST_DOCS_REPO $TRAVIS_BUILD_DIR/test-deploy
+fi
 
 # build some test docs
 make -C $TEST_DOCS_DIR html && mv ${TEST_DOCS_DIR}/_build/html $TRAVIS_BUILD_DIR/html
