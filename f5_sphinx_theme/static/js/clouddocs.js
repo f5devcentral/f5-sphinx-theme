b64b18f617c4c0df9fd72fd9f3219648a3e80553
Jodie Putrino <j.putrino@f5.com>
2018-06-06 15:02:37 -0600

    Point to clouddocs.f5networks.net for header and footer; NEED TO REPLACE THIS WITH PROD URL ONCE HTTPS IS UP AND RUNNING

---
+++ f5_sphinx_theme/static/js/clouddocs.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    $("#clouddocs-header").load("https://clouddocs.f5networks.net/header.html");
+    $("#clouddocs-footer").load("https://clouddocs.f5networks.net/footer.html");
+});
