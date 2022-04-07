f6d79a07b26046c265f0b179674bc0c465279e51
aljain <a.jain@f5.com>
2022-04-07 14:13:59 -0700

    fix pypi deploy

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         ]
     },
     include_package_data=True,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=3.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         'Framework :: Sphinx',
         'Framework :: Sphinx :: Theme',
